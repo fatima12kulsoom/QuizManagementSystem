cfb79bd226cc6d5a991e2868b39ff0d03f595d47
fatima12kulsoom <baqarm216@gmail.com>
2026-08-20 02:24:36 +0500

    Refactor Student class by removing methods
    Removed display and update methods for the Student class.

--- Student.cs
+++ Student.cs
@@ -22,33 +22,5 @@ class Student
 
         Console.Write("Enter Department: ");
         Department = Console.ReadLine();
-
-        Console.WriteLine("\nStudent Registered Successfully!");
-    }
-
-    public void DisplayStudent()
-    {
-        Console.WriteLine("----------------------------");
-        Console.WriteLine("Student ID : " + StudentID);
-        Console.WriteLine("Name       : " + Name);
-        Console.WriteLine("Roll No    : " + RollNo);
-        Console.WriteLine("Department : " + Department);
-        Console.WriteLine("----------------------------");
-    }
-
-    public void UpdateStudent()
-    {
-        Console.WriteLine("\nUpdate Student");
-
-        Console.Write("Enter New Name: ");
-        Name = Console.ReadLine();
-
-        Console.Write("Enter New Roll No: ");
-        RollNo = Console.ReadLine();
-
-        Console.Write("Enter New Department: ");
-        Department = Console.ReadLine();
-
-        Console.WriteLine("Student Updated Successfully!");
     }
 }
